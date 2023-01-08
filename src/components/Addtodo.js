5362216d8f7d08660d788eeb11f31b37e6060fed
Ismail Chahir <ismailchahir01@gmail.com>
2023-01-08 15:20:24 +0100

    adding some functionality

--- src/components/Addtodo.js
+++ src/components/Addtodo.js
@@ -1,22 +1,55 @@
 import React, { useState } from "react";
 
 export default function Addtodo() {
-  const [todo, settodo] = useState({ id: "0", value: "" });
-  function submitTodo(e) {}
+  const [todos, setTodo] = useState([]);
+  const [input, setInput] = useState();
+  const [message, setMessage] = useState("What task you have to do ?");
+  function handleClick() {
+    if (input.trim().length === 0) {
+      setMessage("please fill the input with your task");
+    } else {
+      const id = todos.length + 1; // id of each item to get keys
+      setTodo((prevState) => [
+        ...prevState,
+        {
+          // adding item for OUR 'TO DO LIST'
+          id: id,
+          taskName: input,
+          completed: false,
+        },
+      ]);
+      setInput(""); // empty the enput
+    }
+  }
   return (
-    <form>
-      <input
-        type="text"
-        className="input-todo"
-        placeholder="What do you need to do ?"
-      ></input>
-      <button className="btn-add" onClick={submitTodo}>
-        Add
-      </button>
-      <div className="todos">
-        <p className="todo-text">{todo.value}</p>
-        <button className="btn-end">END task</button>
+    <div>
+      <h3>{message}</h3>
+      <div className="input_todo">
+        <textarea
+          type="text"
+          name="todo"
+          value={input}
+          onInput={(e) => {
+            setInput(e.target.value); // to let the user write inside the input
+          }}
+        />
+        <button className="btn-add" onClick={handleClick}>
+          Add
+        </button>
       </div>
-    </form>
+
+      <div className="todos">
+        <ol>
+          {todos.map((todo) => {
+            // return each item when add button is clicked
+            return (
+              <div className="todolist-item">
+                <li key={todo.id}>{todo.taskName}</li>
+              </div>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
   );
 }
