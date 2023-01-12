e886a56cf052e69aef9ff2b543dc2ad654d37998
Ismail Chahir <ismailchahir01@gmail.com>
2023-01-12 10:28:16 +0100

    version 1.0

--- src/components/Addtodo.js
+++ src/components/Addtodo.js
@@ -1,54 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid"; // generating values for keys
 
 export default function Addtodo() {
-  const [todos, setTodo] = useState([]);
+  const [todos, setTodo] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
   const [input, setInput] = useState();
   const [message, setMessage] = useState("What task you have to do ?");
+  // click to add todo item to the list
   function handleClick() {
     if (input.trim().length === 0) {
       setMessage("please fill the input with your task");
     } else {
-      const id = todos.length + 1; // id of each item to get keys
+      const id = todos.length + 1; // id of each item to get keys and put number of Todo
       setTodo((prevState) => [
         ...prevState,
         {
           // adding item for OUR 'TO DO LIST'
           id: id,
           taskName: input,
-          completed: false,
         },
       ]);
-      setInput(""); // empty the enput
+      setMessage("do you have some other To Do's ? ADD IT");
+      setInput(""); // empty the input
     }
   }
+  function deleteToDo(id) {
+    setTodo((prevState) => {
+      // delete toDo clicked
+      return prevState.filter((todo) => todo.id !== id);
+    });
+  }
+  const enterToAdd = (e) => {
+    if (e.keyCode === 13) {
+      handleClick(); // press enter to add toDo
+    }
+  };
+  // local storage function
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  });
   return (
-    <div>
+    <div className="flex">
       <h3>{message}</h3>
       <div className="input_todo">
-        <textarea
+        <input
           type="text"
           name="todo"
-          value={input}
+          value={input || ""}
           onInput={(e) => {
             setInput(e.target.value); // to let the user write inside the input
           }}
+          onKeyDown={enterToAdd}
         />
         <button className="btn-add" onClick={handleClick}>
-          Add
+          &#8330;
         </button>
       </div>
 
       <div className="todos">
-        <ol>
+        <ul>
           {todos.map((todo) => {
             // return each item when add button is clicked
             return (
-              <div className="todolist-item">
-                <li key={todo.id}>{todo.taskName}</li>
+              <div
+                className="todolist-item"
+                key={uuidv4()}
+                onClick={() => deleteToDo(todo.id)}
+              >
+                <li>
+                  <span>&#128204;</span>
+                  {todo.taskName}
+                </li>
               </div>
             );
           })}
-        </ol>
+        </ul>
       </div>
     </div>
   );
